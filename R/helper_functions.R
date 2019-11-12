6246892092f50412372f6f7066d5a56dc4fc2f96
rjake <rjake@sas.upenn.edu>
2019-11-12 12:33:13 -0500

    Remove unused fields

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,27 +1,15 @@
+# Used to expand the range to a new min and max
 change_range <- function(x, new_min, new_max) {
   (x - min(x)) / (max(x) - min(x)) * (new_max - new_min) + new_min
 }
 
-
-check_cut <- function(x) {
-  (is.numeric(x) | is.integer(x)) & n_distinct(x) > 15
-}
-
-
-check_n_cat <- function(x) {
-  n <- n_distinct(x)
-
-  n != 1 &
-    (is.character(x) | is.factor(x)) &
-    n < 100 &
-    n < length(x)
-}
 
 # confirm if numeric/date fields should be cut
 check_num_cat <- function(x, n_quantile) {
   (is.numeric(x) | is.integer(x) | lubridate::is.Date(x)) &
     n_distinct(x) > n_quantile
 }
+
 
 # cut numeric and dates into 10 groups
 cut_custom <- function(x, n_quantile, order = FALSE) {
@@ -45,6 +33,7 @@ cut_custom <- function(x, n_quantile, order = FALSE) {
 
   label
 }
+
 
 # lump categorical data into 10 groups
 collapse_cat <- function(x, n) {
