590bff3d24b3e7866f16eaeb1952c1bcf72981f9
rjake <rjake@sas.upenn.edu>
2019-11-12 22:25:46 -0500

    Add tests to variation plot

--- tests/testthat/test-variation-plot.R
+++ tests/testthat/test-variation-plot.R
@@ -1,3 +1,24 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("variation_plot works", {
+  p <- variation_plot(iris, "Petal.Length")
+
+  actual_dim <- dim(p$data)
+  expected_dim <- c(28, 10)
+  expect_equal(actual_dim, expected_dim)
+
+  actual_layers <- length(p$layers)
+  expected_layers <- 3
+  expect_equal(actual_layers, expected_layers)
+})
+
+
+test_that("variation_plot_single_obs works", {
+  p <- variation_plot_single_obs(iris, "Petal.Length")
+
+  actual_dim <- dim(p$data)
+  expected_dim <- c(28, 10)
+  expect_equal(actual_dim, expected_dim)
+
+  actual_layers <- length(p$layers)
+  expected_layers <- 6
+  expect_equal(actual_layers, expected_layers)
 })
