6023cf2f9dcb851f628274a3bd81b28a86985aff
rjake <rjake@sas.upenn.edu>
2019-11-12 21:55:22 -0500

    Remove date logic

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -6,9 +6,7 @@ change_range <- function(x, new_min, new_max) {
 
 # confirm if numeric/date fields should be cut
 check_cut_numeric <- function(x, n_quantile) {
-  (is.numeric(x) | is.integer(x) |
-     lubridate::is.Date(x) | lubridate::is.POSIXct(x)
-   ) &
+  (is.numeric(x) | is.integer(x)) &
     n_distinct(x) > n_quantile
 }
 
