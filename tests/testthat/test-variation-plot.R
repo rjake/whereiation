46e4526b96c5425934f21422e3183850b4fc9618
rjake <rjake@sas.upenn.edu>
2019-11-12 22:57:36 -0500

    Update test-variation-plot.R

--- tests/testthat/test-variation-plot.R
+++ tests/testthat/test-variation-plot.R
@@ -12,13 +12,18 @@ test_that("variation_plot works", {
 
 
 test_that("variation_plot_single_obs works", {
-  p <- variation_plot_single_obs(iris, "Petal.Length")
-
-  actual_dim <- dim(p$data)
+  p1 <- variation_plot_single_obs(iris, "Petal.Length", avg_type = "mean")
+  p2 <- variation_plot_single_obs(iris, "Petal.Length", labels = TRUE)
+  actual_dim_p1 <- dim(p1$data)
+  actual_dim_p2 <- dim(p2$data)
   expected_dim <- c(28, 10)
-  expect_equal(actual_dim, expected_dim)
+  expect_equal(actual_dim_p1, actual_dim_p2, expected_dim)
 
-  actual_layers <- length(p$layers)
-  expected_layers <- 6
-  expect_equal(actual_layers, expected_layers)
+  actual_layers_p1 <- length(p1$layers)
+  expected_layers_p1 <- 6
+  expect_equal(actual_layers_p1, expected_layers_p1)
+
+  actual_layers_p2 <- length(p2$layers)
+  expected_layers_p2 <- 7
+  expect_equal(actual_layers_p2, expected_layers_p2)
 })
