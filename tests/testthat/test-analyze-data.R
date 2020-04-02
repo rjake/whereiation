b9c347e615a2d651e6220a80b5fc689756cf977f
rjake <rjake@sas.upenn.edu>
2020-04-01 23:42:24 -0400

    Move test to separate script

--- tests/testthat/test-analyze-data.R
+++ tests/testthat/test-analyze-data.R
@@ -42,23 +42,3 @@ test_that("analyze_data returns list", {
   expect_true(is.list(x))
 })
 
-test_that("calculate_factor_stats works", {
-  df <- calculate_factor_stats(df = iris, dep_var = "Petal.Length")
-
-  actual_dim <- dim(df)
-  expected_dim <- c(586, 14)
-  expect_equal(actual_dim, expected_dim)
-
-  actual_names <- names(df)
-  expected_names <-
-    c("datascanr_outcome", "datascanr_id", "field", "value",
-      "group_avg", "n",
-      "field_r_sq", "field_r_sq_adj", "field_p_value",
-      "grand_avg",
-      "field_wt", "complete",
-      "group_avg_wt",
-      "estimate")
-
-  expect_equal(actual_names, expected_names)
-})
-
