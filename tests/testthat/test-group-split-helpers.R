47f62e2f4e4d0ab534090dc7396ab4ed41f34baa
rjake <rjake@sas.upenn.edu>
2020-07-11 17:43:34 -0400

    Add test for helpers

---
+++ tests/testthat/test-group-split-helpers.R
@@ -0,0 +1,97 @@
+group_split
+
+
+test_that("over_under_split", {
+  df <-
+    over_under_split(
+      df = refactor_columns(ggplot2::mpg, "hwy", split = "year"),
+      field = "class",
+      type = "dv"
+    )
+
+  expected_dim <- c(7, 12)
+  actual_dim <- dim(df)
+  expect_equal(expected_dim, actual_dim)
+})
+
+
+test_that("group_split_prep", {
+  df <-
+    group_split_prep(ggplot2::mpg, dep_var = "hwy", split = "year", type = "dv")
+
+  expected_dim <- c(70, 12)
+  actual_dim <- dim(df)
+  expect_equal(expected_dim, actual_dim)
+})
+
+
+test_that("group_split_plot_data", {
+  df <-
+    group_split_plot_data(
+      base_data =
+        group_split_prep(
+          df = ggplot2::mpg,
+          split_on = "year",
+          type = "dv",
+          dep_var = "hwy"
+        ),
+      threshold = 1,
+      ref_group = "1",
+      trunc_length = 20
+    )
+
+  expected_dim <- c(41, 17)
+  actual_dim <- dim(df)
+  expect_equal(expected_dim, actual_dim)
+
+  expect_equal(
+    object = max(nchar(as.character(df$value))),
+    expected = 20
+  )
+})
+
+
+test_that("group_split_counts", {
+  df <-
+    group_split_counts(
+      base_data =
+        group_split_prep(
+          df = ggplot2::mpg,
+          split_on = "year",
+          type = "dv",
+          dep_var = "hwy"
+        ),
+      ref_group = "1",
+      split_on = "year"
+    )
+
+  expected_dim <- c(2, 6)
+  actual_dim <- dim(df)
+  expect_equal(expected_dim, actual_dim)
+})
+
+
+
+test_that("check_dv_has_value", {
+  expect_equal(
+    object = check_dv_has_value(type = "count"),
+    expected = "1"
+  )
+
+  expect_equal(
+    object = check_dv_has_value(type = "percent"),
+    expected = "1"
+  )
+
+  expect_equal(
+    object = check_dv_has_value(type = "dv", FALSE, "x"),
+    expected = "x"
+  )
+
+  expect_error(
+    object = check_dv_has_value(type = "dv", TRUE),
+    regexp = "required"
+  )
+
+})
+
