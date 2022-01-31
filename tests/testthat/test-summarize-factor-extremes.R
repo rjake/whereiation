b8dc30e8443ad019da175582f03ca4c46360c52b
Jake <rjake@sas.upenn.edu>
2022-01-31 00:45:51 -0500

    Update tests

--- tests/testthat/test-summarize-factor-extremes.R
+++ tests/testthat/test-summarize-factor-extremes.R
@@ -1,6 +1,6 @@
 test_that("summarize_factor_extremes works", {
   df <- summarize_factor_extremes(df = iris, dv = Petal.Length)
-  expected_dim <- c(4, 9)
+  expected_dim <- c(4, 8)
   actual_dim <- dim(df)
   expect_equal(expected_dim, actual_dim)
 })
